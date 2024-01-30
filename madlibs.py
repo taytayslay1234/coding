159749a3171a8f3306e6d669a1123698865a942b
taytayslay1234 <157722159+taytayslay1234@users.noreply.github.com>
2024-01-30 15:18:36 +0000

    done

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,43 @@
+words=[]
 
+answer = input("GIVE ME AN ADJICTIVE")
+
+words.append(answer)
+
+answer = input("GIVE ME A STORE")
+
+words.append(answer)
+
+answer = input("GIVE ME A NOUN")
+
+words.append(answer)
+
+answer = input("GIVE ME A NOUN")
+
+words.append(answer)
+
+answer = input("GIVE ME A ADVERB")
+
+words.append(answer)
+
+answer = input("GIVE ME A VERB")
+
+words.append(answer)
+
+answer = input("GIVE ME A VERB")
+
+words.append(answer)
+
+answer = input("GIVE ME A PLACE")
+
+words.append(answer)
+
+answer = input("GIVE ME A NOUN")
+
+words.append(answer)
+
+answer = input("GIVE ME AN ADJECTIVE ENDING IN ED")
+
+words.append(answer)
+
+print(f"A {words[0]} person went to {words[1]} to get a {words[2]}. they liked the {words[3]} and went to the shop to {words[4]} {words[5]}. then they {words[6]} to {words[7]} for a {words[8]}. They {words[9]} it.")
